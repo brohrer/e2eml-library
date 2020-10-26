3fed3108153bd33b8e40267906ad7fcf6c9c9162
brohrer <brohrer@gmail.com>
2020-10-26 19:13:21 -0400

    header and footer edits

--- javascripts/blog_footer.js
+++ javascripts/blog_footer.js
@@ -4,16 +4,11 @@ document.write('\
       <footer class="inner">\
         <section class="clearfix">\
           <a id="project_tagline" href="http://e2eml.school/courses">\
-          <h4>Visit the e2eML course catalog</h4>\
+          <h4>Visit the e2eML course catalog</h4></a>\
           <p>\
-            Wherever you are in your machine learning journey\
-            The End to End Machine Learning School has video and\
-            project tutorials to take you to the next level.\
-          </p>\
-          <p>\
-            Come on over for a visit.\
-          </p>\
-          <p>\
+            The End to End Machine Learning School is\
+            a collection of project-centered tutorials designed to\
+            help you on your applied machine learning journey.\
             <br>\
             <a href="https://creativecommons.org/publicdomain/zero/1.0/">\
               <img title="Creative Commons Zero license" src="images/cc0.png" alt="CC0" style="width: 90px;" />\
