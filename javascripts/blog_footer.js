01759c7603db2ddb20d97e66e6ff7aca2a56a56d
brohrer <brohrer@gmail.com>
2021-08-19 18:31:01 -0400

    color palette update

--- javascripts/blog_footer.js
+++ javascripts/blog_footer.js
@@ -3,17 +3,8 @@ document.write('\
     <div id="footer_wrap" class="outer">\
       <footer class="inner">\
         <section class="clearfix">\
-          <a id="footer_message" href="http://e2eml.school/courses">\
           <p>\
-            The End to End Machine Learning School is\
-            a collection of project-centered tutorials designed to\
-            help you on your applied machine learning journey.\
-          </p>\
-          <p>Come visit the e2eML course catalog.</p></a>\
-            <br>\
-            <a href="https://creativecommons.org/publicdomain/zero/1.0/">\
-              <img title="Creative Commons Zero license" src="images/cc0.png" alt="CC0" style="width: 90px;" />\
-            </a> &nbsp except where noted.\
+            All text and images CC0 except where noted.\
           </p>\
           <p>\
             The postings on this site are my own and don\'t necessarily\
--- javascripts/blog_header.js
+++ javascripts/blog_header.js
@@ -3,11 +3,6 @@ document.write('\
     <div id="header_wrap" class="outer">\
         <header class="inner">\
           <h1 id="project_title">' + blog_title + '</h1>\
-            <h4>\
-              <a id="project_tagline" href="https://e2eml.school/catalog">\
-                Find your course at the e2eML school\
-              </a>\
-            </h4>\
         </header>\
     </div>\
 ');
