8ad2a3b6c3ec82cd3042ca2c08377089a0e93f39
brohrer <brohrer@gmail.com>
2021-08-19 18:45:47 -0400

    color palette update

--- javascripts/blog_header.js
+++ javascripts/blog_header.js
@@ -4,8 +4,8 @@ document.write('\
         <header class="inner">\
           <h1 id="project_title">' + blog_title + '</h1>\
             <h4>\
-              <a id="project_tagline" href="https://e2eml.school/catalog">\
-                Find your course at the e2eML school\
+              <a id="project_tagline" href="https://e2eml.school/">\
+                Brandon Rohrer\
               </a>\
             </h4>\
         </header>\
