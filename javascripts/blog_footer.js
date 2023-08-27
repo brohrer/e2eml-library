2bd112ac82414522c534ff2f68a735c675ba0098
brohrer <brohrer@gmail.com>
2023-08-27 18:53:52 -0400

    RSS feed added

--- javascripts/blog_footer.js
+++ javascripts/blog_footer.js
@@ -4,6 +4,15 @@ document.write('\
       <footer class="inner">\
         <section class="clearfix">\
           <p>\
+            <a href="https://brandonrohrer.com/feed.xml">\
+            Subscribe to updates 
+              <img title="RSS icon"
+                src="images/rss_icon.png"
+                alt="The canonical RSS icon"
+                style="height: 40px;">
+            </a>\
+          </p>\
+          <p>\
             <a href="https://creativecommons.org/publicdomain/zero/1.0/">\
             All text and images CC0 except where noted.</a>\
           </p>\
